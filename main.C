8f0be9fc47a844f4acca8884f97f1d5b000c0101
Matt Stine <stine.matt@gmail.com>
2011-12-15 22:04:44 -0800

    Create basic Grid, Cell, and Vector utility classes.  Document algorithm.

--- main.C
+++ main.C
@@ -1,8 +1,20 @@
 #include <iostream>
+#include "Grid.H"
 
 int main(int argc, char **argv)
 {
-  
+  // 0. Set initial simulation state.
+  // 1. Calculate the simulation time step based on max velocity (CFL Condition)
+  // 2. Advance the velocity field.
+  //    2a. Apply convection using backward particle trace and RK2
+  //    2b. Apply external forces, inc gravity.
+  //    2c. Run the pressure solve through MUMPS.
+  //    2d. Apply the pressure to the simulation.
+  //    2e. Nullify "solid cell" velocities.
+  // 3. Move visible particles through the velocity field.
 
+  // TODO, create Fluid class to encapsulate Grid and Particles.
+  Grid2D<float> grid(128, 128);
+  
   return 0;
 }
