2d9d548dcc456fa0cfda4ac675beaed2f486100b
Matt Stine <stine.matt@gmail.com>
2012-01-02 16:48:29 -0800

    Add logic to connect all Grid neighbors to a specific cell.

--- Grid.cpp
+++ Grid.cpp
@@ -11,48 +11,64 @@ Grid2D::Grid2D(unsigned rowCount, unsigned colCount)
 {
   // Initialize data members.
   _cells.resize(_rowCount * _colCount);
-  
+
   // Create linkage between cells, since we're delegating velocity value
   // interpolation to the cell objects themselves.
+  Cell2D *neighbors[Cell2D::NEIGHBOR_COUNT];
   unsigned size = _rowCount * _colCount;
   for (unsigned i = 0; i < size; ++i) {
-    Cell2D *neighbors[Cell2D::NEIGHBOR_COUNT];
 
-    // Top Left Neighborx
-    // TODO
-    neighbors[Cell2D::TOP_LEFT] = NULL;
+    unsigned row = i / _colCount;
+    unsigned col = i % _colCount;
+
+    // Initialize all neighbors to a NULL pointer.
+    for (unsigned n = 0; n < Cell2D::NEIGHBOR_COUNT; ++n) {
+      neighbors[n] = NULL;
+    }
+
+    // Top Left Neighbor
+    if (row != _rowCount - 1 &&
+	col != 0) {
+      neighbors[Cell2D::TOP_LEFT] = &(_cells[i + _colCount - 1]);
+    }
 
     // Top Neighbor
-    if (i / _colCount != _rowCount - 1) {
+    if (row != _rowCount - 1) {
       neighbors[Cell2D::TOP_CENTER] = &(_cells[i + _colCount]);
     }
 
     // Top Right Cell
-    // TODO
-    neighbors[Cell2D::TOP_RIGHT] = NULL;
+    if (row != _rowCount - 1 &&
+	col != _colCount - 1) {
+      neighbors[Cell2D::TOP_RIGHT] = &(_cells[i + _colCount + 1]);
+    }
     
     // Left Neighbor
-    if (i % _colCount != 0) {
+    if (col != 0) {
       neighbors[Cell2D::LEFT] = &(_cells[i - 1]);
     }
 
     // Right Neighbor
-    if (i % _colCount != _colCount - 1) {
+    if (col != _colCount - 1) {
       neighbors[Cell2D::RIGHT] = &(_cells[i + 1]);
     }
     
     // Bottom Left Neighbor
-    // TODO
-    neighbors[Cell2D::BOTTOM_LEFT] = NULL;
+    if (row != 0 &&
+	col != 0) {
+      neighbors[Cell2D::BOTTOM_LEFT] = &(_cells[i - _colCount - 1]);
+    }
 
     // Bottom Neighbor
-    if (i / _colCount != 0) {
+    if (row != 0) {
       neighbors[Cell2D::BOTTOM_CENTER] = &(_cells[i - _colCount]);
     }
 
     // Bottom Right Neighbor
-    // TODO
-    neighbors[Cell2D::BOTTOM_RIGHT] = NULL;
+    if (row != 0 &&
+	col != _colCount - 1) {
+      neighbors[Cell2D::BOTTOM_RIGHT] = &(_cells[i - _colCount + 1]);
+    }
 
     // Set the cell linkage.
     _cells[i].setLinkage(neighbors, Cell2D::NEIGHBOR_COUNT);
