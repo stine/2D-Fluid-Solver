824522ba8d6c4557a7cce210bfe334f6a6620133
Jason Burton <jas.burton@gmail.com>
2012-02-19 22:27:35 -0800

    Implement advection.  Boundary conditions are enforced explicity inside of advectVelocity.  Find a better method of approach for this.  Modify window size and grid size.

--- main/main.cpp
+++ main/main.cpp
@@ -22,8 +22,8 @@ int main(int argc, char *argv[])
   // Note: values in this velocity field are arbitrarily chosen and may be
   //  divergent within a cell.
   // Note: sin() is used to clamp output values to [-1, 1].
-  float width = 3.0f;
-  float height = 3.0f;
+  float width = 15.0f;
+  float height = 15.0f;
   Grid grid(width, height);
   for (float y = 0; y < height; ++y) 
     for (float x = 0; x < width; ++x) {
